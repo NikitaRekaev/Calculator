221cf0196a0278cf41a01e9a59b2a9d6a2a14f4e
NikitaRekaev <nvvvvk@gmail.com>
2023-04-29 01:23:46 +0400

    feat: constants for home view added

--- Calculator/Module/View/HomeView.m
+++ Calculator/Module/View/HomeView.m
@@ -3,6 +3,19 @@
 
 #import "HomeView.h"
 
+#pragma mark - Constants
+
+#define buttonTitles @[@"0", @",", @"=", @"1", @"2", @"3", @"+", @"4", @"5", @"6", @"−", @"7", @"8", @"9", @"×", @"AC", @"±", @"%", @"÷"]
+#define buttonFontSize 36
+#define buttonStandartSize self.frame.size.width / 4.7
+#define buttonSpacing  10
+#define labelFontSize 72
+#define labelPadding 30
+#define labelBottom 20
+#define labelHeight 60
+
+#pragma mark - Interface
+
 @interface HomeView (private)
 
 @property (nonatomic, strong) UILabel *outputLabel;
@@ -11,12 +24,13 @@
 
 @end
 
+#pragma mark - Implementation
 
 @implementation HomeView
 
 - (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
-    self.titles = @[@"0", @",", @"=", @"1", @"2", @"3", @"+", @"4", @"5", @"6", @"−", @"7", @"8", @"9", @"×", @"AC", @"±", @"%", @"÷"];
+    self.titles = buttonTitles;
     return self;
 }
 
@@ -27,51 +41,55 @@
     [self configureOutputLabel];
 }
 
+#pragma mark - Actions
+
+- (void)buttonTapped:(UIButton *)sender {
+
+}
+
+#pragma mark - Configure label
+
 - (void)configureOutputLabel {
     self.outputLabel = [[UILabel alloc] init];
     self.outputLabel.translatesAutoresizingMaskIntoConstraints = NO;
-    self.outputLabel.font = [UIFont systemFontOfSize: 72];
+    self.outputLabel.font = [UIFont systemFontOfSize: labelFontSize];
     self.outputLabel.textColor = [UIColor whiteColor];
     self.outputLabel.textAlignment = NSTextAlignmentRight;
-    self.outputLabel.text = @"0";
+    self.outputLabel.text = buttonTitles.firstObject;
     [self addSubview: self.outputLabel];
     UIButton *lastButton = [self.buttons lastObject];
 
     [NSLayoutConstraint activateConstraints:@[
-        [self.outputLabel.bottomAnchor constraintEqualToAnchor: lastButton.topAnchor constant:-20],
-        [self.outputLabel.leftAnchor constraintEqualToAnchor:self.leftAnchor constant:30],
-        [self.outputLabel.rightAnchor constraintEqualToAnchor:self.rightAnchor constant:-30],
-        [self.outputLabel.heightAnchor constraintEqualToConstant:60]
+        [self.outputLabel.bottomAnchor constraintEqualToAnchor: lastButton.topAnchor constant:-labelBottom],
+        [self.outputLabel.leftAnchor constraintEqualToAnchor:self.leftAnchor constant:labelPadding],
+        [self.outputLabel.rightAnchor constraintEqualToAnchor:self.rightAnchor constant:-labelPadding],
+        [self.outputLabel.heightAnchor constraintEqualToConstant:labelHeight]
     ]];
 }
 
+#pragma mark - Configure buttons
+
 - (void)configureButtons {
     self.buttons = [NSMutableArray array];
-    CGFloat x = 10;
-    CGFloat y = -30;
+    CGFloat x = buttonSpacing;
+    CGFloat y = -labelPadding;
 
     for (NSString *title in self.titles) {
-        [self createButton:title];
-    }
-
-    for (UIButton *button in self.buttons) {
+        UIButton *button = [self createButton:title];
         [self setConstraintForButton:button x:&x y:&y];
     }
 }
 
-- (void)buttonTapped:(UIButton *)sender {
-    
-}
-
-- (void)createButton:(NSString *)title {
+- (UIButton *)createButton:(NSString *)title {
     UIButton *button = [UIButton buttonWithType: UIButtonTypeSystem];
     [button setTitle: title forState: UIControlStateNormal];
-    button.titleLabel.font = [UIFont systemFontOfSize: 36];
-    button.layer.cornerRadius = self.frame.size.width / 9.4;
+    button.titleLabel.font = [UIFont systemFontOfSize: buttonFontSize];
+    button.layer.cornerRadius = buttonStandartSize / 2;
     [button addTarget: self action: @selector(buttonTapped:) forControlEvents: UIControlEventTouchUpInside];
     [self addSubview:button];
     [self.buttons addObject:button];
     [self setColorForButton: button];
+    return button;
 }
 
 - (void)setColorForButton:(UIButton *)button {
@@ -94,9 +112,9 @@
 }
 
 - (void)setConstraintForButton:(UIButton *)button x:(CGFloat *)x y:(CGFloat *)y {
-    CGFloat buttonHeight = self.frame.size.width / 4.7;
-    CGFloat buttonWidth = self.frame.size.width / 4.7;
-    CGFloat spacing = 10;
+    CGFloat buttonHeight = buttonStandartSize;
+    CGFloat buttonWidth = buttonStandartSize;
+    CGFloat spacing = buttonSpacing;
 
     button.translatesAutoresizingMaskIntoConstraints = NO;
 
