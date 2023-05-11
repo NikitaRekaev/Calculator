f4a22f21da6d153ef4accae02d3ae2544a63d2c6
NikitaRekaev <nvvvvk@gmail.com>
2023-05-11 15:42:16 +0400

    ref: view model refactored

--- Calculator/Module/ViewModel/CalculatorViewModel.m
+++ Calculator/Module/ViewModel/CalculatorViewModel.m
@@ -7,6 +7,7 @@
 #pragma mark - Constants
 
 #define buttonTitles @[@"0", @",", @"=", @"1", @"2", @"3", @"+", @"4", @"5", @"6", @"−", @"7", @"8", @"9", @"×", @"AC", @"±", @"%", @"÷"]
+#define format @"%.9g"
 
 
 #pragma mark - Interface
@@ -14,9 +15,9 @@
 @interface CalculatorViewModel ()
 
 @property (nonatomic, strong) NSString *outputString;
-@property (nonatomic, assign) BOOL isNegative;
-@property (nonatomic, strong) NSString *firstValue;
-@property (nonatomic, strong) NSString *secondValue;
+@property (nonatomic) BOOL isNegative;
+@property double firstValue;
+@property double secondValue;
 @property char operator;
 
 @end
@@ -49,17 +50,55 @@
         _outputString = [_outputString stringByAppendingString:value];
     }
 
-    if (_operator == 0) {
-        _firstValue = _outputString;
-    } else {
-        _secondValue = _outputString;
-    }
-
+    [self addValue:_outputString];
     [_view updateValue:_outputString];
 }
 
 
 - (void)operatorButtonPressed:(NSString *)value {
+    [self addOperator:value];
+    _isNegative = NO;
+    _outputString = @"";
+}
+
+- (void)percentButtonPressed:(NSString *)value {
+    _outputString = [NSString stringWithFormat:format, [_outputString floatValue] * 0.01];
+    [_view updateValue:_outputString];
+}
+
+- (void)negateButtonPressed:(NSString *)value {
+    if ([_outputString isEqualToString:@"0"] || [_outputString isEqualToString:@""]) {
+        return;
+    }
+
+    [self setIsNegative];
+    [_view updateValue:_outputString];
+}
+
+- (void)clearButtonPressed:(NSString *)value {
+    [self clear];
+    [_view updateValue:_outputString];
+}
+
+- (void)resultButtonPressed:(NSString *)value {
+    double calculatedValue = [self calculate];
+    [self setResult:calculatedValue];
+    [_view updateValue:_outputString];
+}
+
+
+#pragma mark - Private methods
+
+- (void)addValue:(NSString *)value {
+    double selfValue = [value doubleValue];
+    if (_operator == 0) {
+        _firstValue = selfValue;
+    } else {
+        _secondValue = selfValue;
+    }
+}
+
+- (void)addOperator:(NSString *)value {
     if ([value  isEqual: @"+"]) {
         _operator = '+';
     } else if ([value  isEqual: @"−"]) {
@@ -69,21 +108,9 @@
     } else if ([value  isEqual: @"÷"]) {
         _operator = '/';
     }
-
-    _isNegative = NO;
-    _outputString = @"";
 }
 
-- (void)percentButtonPressed:(NSString *)value {
-    _outputString = [NSString stringWithFormat:@"%.9g", [_outputString floatValue] * 0.01];
-    [_view updateValue:_outputString];
-}
-
-- (void)negateButtonPressed:(NSString *)value {
-    if ([_outputString isEqualToString:@"0"] || [_outputString isEqualToString:@""]) {
-        return;
-    }
-
+- (void)setIsNegative {
     if (_isNegative) {
         _outputString = [_outputString substringFromIndex:1];
         _isNegative = NO;
@@ -91,63 +118,56 @@
         _outputString = [@"−" stringByAppendingString:_outputString];
         _isNegative = YES;
     }
-
-    [_view updateValue:_outputString];
 }
 
-- (void)clearButtonPressed:(NSString *)value {
-    _isNegative = NO;
-    _firstValue = nil;
-    _secondValue = nil;
-    _operator = 0;
-    _outputString = @"0";
-    [_view updateValue:_outputString];
-}
-
-- (void)resultButtonPressed:(NSString *)value {
-    [self calculate];
-    [_view updateValue:_outputString];
-}
-
-
-#pragma mark - Private methods
-
-- (void)calculate {
+- (double)calculate {
     double value = 0.0;
     switch (_operator) {
         case '+':
-            value = [_firstValue doubleValue] + [_secondValue doubleValue];
+            value = _firstValue + _secondValue;
             break;
         case '-':
-            value = [_firstValue doubleValue] - [_secondValue doubleValue];
+            value = _firstValue - _secondValue;
             break;
         case '*':
-            value = [_firstValue doubleValue] * [_secondValue doubleValue];
+            value = _firstValue * _secondValue;
             break;
         case '/':
-            if ([_secondValue doubleValue] == 0.0) {
+            if (_secondValue == 0.0) {
                 value = NAN;
             } else {
-                value = [_firstValue doubleValue] / [_secondValue doubleValue];
+                value = _firstValue / _secondValue;
             }
             break;
         default:
             break;
     }
 
+    return value;
+}
+
+- (void)setResult:(double)value {
     if (isnan(value) || isinf(value)) {
         _outputString = @"Error";
     } else {
-        _outputString = [NSString stringWithFormat:@"%.9g", value];
+        _outputString = [NSString stringWithFormat:format, value];
     }
 
     if (_isNegative) {
         _outputString = [@"−" stringByAppendingString:_outputString];
     }
 
-    _firstValue = _outputString;
-    _secondValue = nil;
+    _firstValue = value;
+    _secondValue = 0;
     _operator = 0;
+}
+
+- (void)clear {
+    _isNegative = NO;
+    _firstValue = 0;
+    _secondValue = 0;
+    _operator = 0;
+    _outputString = @"0";
 }
 
 @end
